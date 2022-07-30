4cf96bb5ff823526435c1cb59808a88f938e51e4
Chris Rad <chris@cracustom.com>
2022-07-29 23:56:24 -0700

    adding tailwindui as dep and testing on index.html.heez

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :portfolio, PortfolioWeb.Endpoint,
   secret_key_base: "tpfhIJgl8Sbn1Pw/OacT8rwSVFxwyV+T0h3UxthsfNCmN69KhuDgKwVGGx94+Pyj",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
