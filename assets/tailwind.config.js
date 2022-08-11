b1084fca8145b9dda2b6b8010be1d425ae8ac9a7
Chris Rad <chris@cracustom.com>
2022-08-10 23:15:26 -0700

    extending fonts list

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+	fontFamily: {
+	    sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+	    lobster: ['Lobster']
+	}
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
