34c05ab047455ab4f4c898953f8e6dbb61e7af21
Christopher K Rad <chris@cracustom.com>
2022-08-15 23:10:22 -0700

    adding config and links to dartsass

--- config/config.exs
+++ config/config.exs
@@ -57,6 +57,11 @@ config :tailwind, version: "3.1.6", default: [
   cd: Path.expand("../assets", __DIR__)
 ]
 
+# Config for DartSass
+# https://github.com/CargoSense/dart_sass
+# https://pragmaticstudio.com/tutorials/adding-tailwind-css-to-phoenix
+config :dart_sass, version "1.49.11"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
