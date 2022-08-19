e289dfe4a66f1e45b6bf4fa3c8cb865e0954d616
Chris Rad <chris@cracustom.com>
2022-08-19 10:26:28 -0700

    removing custom colors to enable default tailwind colors

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,17 +14,6 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    colors: {
-	'blue': '#2d313a',
-	'purple': '#7e5bef',
-	'pink': '#ff49db',
-	'orange': '#ff7849',
-	'green': '#13ce66',
-	'yellow': '#ffc82c',
-	'gray-dark': '#273444',
-	'gray': '#8492a6',
-	'gray-light': '#d3dce6',
-    },
     extend: {
 	fontFamily: {
 	    sans:['Inter var', ...defaultTheme.fontFamily.sans]
