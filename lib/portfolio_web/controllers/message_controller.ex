22b9e2f660f891768e9c15d4a01e9b8ee8efc70d
Christopher K Rad <chris@cracustom.com>
2022-09-22 15:53:20 -0700

    adding create message method

--- lib/portfolio_web/controllers/message_controller.ex
+++ lib/portfolio_web/controllers/message_controller.ex
@@ -1,7 +1,29 @@
 defmodule PortfolioWeb.MessageController do
   use PortfolioWeb, :controller
 
+  alias Portfolio.Message
+  alias Portfolio.Repo
+
   def index(conn, _params) do
+
     render(conn, "index.html")
+
+  end
+
+  def create(conn, %{"message" => message}) do
+
+    changeset = Portfolio.changeset(%Message{}, message)
+
+    case Repo.insert(changeset) do
+
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Message Received")
+        |> redirect(to: "/thankyou")
+
+      {:error, changeset} ->
+        render(conn, "/message", changeset: changeset)
+
+    end
   end
 end
