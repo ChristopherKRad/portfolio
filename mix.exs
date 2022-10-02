500d218634a1354e2eb1eaf09366bef2e89f611a
Christopher K Rad <chris@cracustom.com>
2022-10-01 19:52:06 -0700

    upgrading to lates phx version

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Portfolio.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.10"},
+      {:phoenix, "~> 1.6.13"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
